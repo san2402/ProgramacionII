236d9a3a454b6891d13ee995919b826ccbe57745
san2402 <san.mallahenriquez@gmail.com>
2017-10-27 20:24:56 -0300

    Array y lista

--- src/ejercicios/TrabajoPractico1.java
+++ src/ejercicios/TrabajoPractico1.java
@@ -60,13 +60,52 @@ public class TrabajoPractico1 {
      * Crear un arreglo de numeros enteros del 1 al 5, recorrerlos y mostralos de las 2 formas
      */
     public void ejercicio3() {
+         int[] numeros = new int[5];
+        numeros[0] = 1;
+        numeros[1] = 3;
+        numeros[2] = 4;
+        numeros[3] = 6;
+        numeros[4] = 7;
         
+        System.out.println("Iterando Arreglos forma 1");
+        for (int i = 0; i < numeros.length; i++) {
+            int numero = numeros[i];
+            System.out.println(numero);
+        }
+        System.out.println("\n");
+        
+        System.out.println("Iterando Arreglos forma 2");
+        for (int numero : numeros) {
+            System.out.println(numero);
+        }
     }
     
     /**
      * Crear una lista de numeros enteros del 1 al 5, recorrerlos y mostarlos de las 3 formas
      */
     public void ejercicio4() {
+         List<Integer> numeros = new ArrayList();
+        numeros.add(1);
+        numeros.add(3);
+        numeros.add(6);
+        numeros.add(9);
+        numeros.add(12);
+        System.out.println("Iterando Listas forma 1");
+        for (int i = 0; i < numeros.size(); i++) {
+            Integer numero = numeros.get(i);
+            System.out.println(numero);
+        }
+        System.out.println("\n");
         
+        System.out.println("Iterando Listas forma 2");
+        for (Integer numero : numeros) {
+            System.out.println(numero);
+        }
+        
+        System.out.println("Iterando Listas forma 3 (Iterador)");
+        Iterator<Integer> iterador = numeros.iterator();
+        while(iterador.hasNext()) {
+            System.out.println(iterador.next());
+        }
     }
 }
--- src/programacion2/Programacion2.java
+++ src/programacion2/Programacion2.java
@@ -1,5 +1,6 @@
 package programacion2;
 
+import ejercicios.TrabajoPractico1;
 import ejercicios.TrabajoPractico3;
 import java.util.List;
 import modelos.DetallePedido;
@@ -8,29 +9,12 @@ import modelos.Pedido;
 
 public class Programacion2 {
 
-    public static void main(String[] args) throws Exception {
-        TrabajoPractico3 practico = new TrabajoPractico3();
+    public static void main(String[] args) {
+       TrabajoPractico1 practico = new TrabajoPractico1();
+       practico.ejercicio4();
         
-        Pedido pedido;
-        
-        pedido = practico.ejercicio2();
-        
-        System.out.println("Nombre Cliente");
-        System.out.println(pedido.getCliente().getNombre());
-        
-        System.out.println("Mercaderia 2");
-        System.out.println(pedido.getDetalles().get(1).getMercaderia().getNombre());
-        
-        List<DetallePedido> listaDetalles = pedido.getDetalles();
-        DetallePedido detallePedido2 = listaDetalles.get(1);
-        Mercaderia mercaderia = detallePedido2.getMercaderia();
-        
-        System.out.println("Mercaderia 2 - Ordenada");
-        System.out.println(mercaderia.getNombre());
-        
-        System.out.println("Cantidad de Botellas");
-        System.out.println(pedido.getDetalles().get(1).getCantidad());
-        
-    }
+    
+    
 
+}
 }
